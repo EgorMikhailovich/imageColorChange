fea2bf6d7a1069ebb2fac03df867857c763d8b61
egormikhailovich <egormikhailovich112@outlook.com>
2021-02-05 23:45:10 +0300

    update image

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ bg.style.height = img.height + 'px'
 
 function myFunction() {
 	var x = document.getElementById("favcolor");
+	
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	var img = document.getElementById("my-image");
+	ctx.drawImage(img, 0, 0);
+	var imgData = ctx.getImageData(0, 0, img.width, img.height);
+
 	var currentVal = x.value;
 	  document.getElementById("demo").innerHTML = currentVal;
 	  const color = currentVal
@@ -13,12 +20,29 @@ function myFunction() {
 	  const g = parseInt(color.substr(3,2), 16)
 	  const b = parseInt(color.substr(5,2), 16)
 	  console.log(`red: ${r}, green: ${g}, blue: ${b}`);
-	  bg.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${randomColor()})`
+	//   bg.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${randomColor()})`
 
+
+	// invert colors
+	var i;
+	for (i = 0; i < imgData.data.length; i += 4) {
+	//  imgData.data[i] = 255 - imgData.data[i];
+	//  imgData.data[i+1] = 255 - imgData.data[i+1];
+	//  imgData.data[i+2] = 255 - imgData.data[i+2];
+	//  imgData.data[i+3] = 255;
+	imgData.data[i] =  (imgData.data[i] + r)%255;
+	imgData.data[i+1] =  (imgData.data[i+1] +g)%255;
+	imgData.data[i+2] =  (imgData.data[i+2] + b)%255;
+	imgData.data[i+3] = 255;
+
+	}
+	ctx.putImageData(imgData, 0, 0);
+	  
+	  
   }
-// function randomColor () {
-// 	return Math.floor(Math.random() * 255)
-// }
+function randomColor () {
+	return Math.floor(Math.random() * 255)
+}
 
 // bg.addEventListener('click', () => {
 // 	bg.style.backgroundColor = `rgba(${randomColor()}, ${randomColor()}, ${randomColor()}, ${randomColor()})`
